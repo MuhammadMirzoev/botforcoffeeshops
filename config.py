5f6daa9782fcc8b32b43c4e0d2a38784f74b9769
MuhammadMirzoev <91388648+MuhammadMirzoev@users.noreply.github.com>
2025-04-19 13:41:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = "7036624062:AAE9W9HaOcRBlh8MfVphgHBuUaSMadxc97I"
-SUPER_ADMINS = [860364840]  # Вставь сюда свой user_id
+TOKEN = "your_token"
+SUPER_ADMINS = [99999999]  # Вставь сюда свой user_id
